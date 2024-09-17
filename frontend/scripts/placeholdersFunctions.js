680ef4fea89df20a6a26dde9dc25e1b3c5513dbc
MateusSousa <alves.mateus1050@gmail.com>
2024-09-17 13:17:28 +0100

    NavBar

---
+++ frontend/scripts/placeholdersFunctions.js
@@ -0,0 +1,17 @@
+function openMenu(){
+
+    console.log("Abrindo menu")
+
+}
+
+function openFilter(){
+    console.log("Abrindo filtro")
+}
+
+function switchPages(){
+    console.log("Trocando de páginas")
+}
+
+function openProfile(){
+    console.log("Abrindo perfil")
+}
